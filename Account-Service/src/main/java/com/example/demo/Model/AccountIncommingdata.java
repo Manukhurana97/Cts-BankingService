4d9757c3cc1b0d98bb12a8df8366c134f19f44fa
Manu khurana <38296197+Manukhurana97@users.noreply.github.com>
2020-08-14 19:27:50 +0530

    Account service

---
+++ Account-Service/src/main/java/com/example/demo/Model/AccountIncommingdata.java
@@ -0,0 +1,49 @@
+package com.example.demo.Model;
+
+public class AccountIncommingdata {
+	
+	public Long accountno;
+	public String ifsc;
+	public String bankname;
+	public String micr;
+	public float amount;
+	
+	
+	
+	public Long getAccountno() {
+		return accountno;
+	}
+	public void setAccountno(Long accountno) {
+		this.accountno = accountno;
+	}
+	public String getIfsc() {
+		return ifsc;
+	}
+	public void setIfsc(String ifsc) {
+		this.ifsc = ifsc;
+	}
+	public String getBankname() {
+		return bankname;
+	}
+	public void setBankname(String bankname) {
+		this.bankname = bankname;
+	}
+	public String getMicr() {
+		return micr;
+	}
+	public void setMicr(String micr) {
+		this.micr = micr;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	
+	
+	
+	
+	
+
+}
